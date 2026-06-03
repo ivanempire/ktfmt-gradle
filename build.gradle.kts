6c7db76f318c4487a4d0f0874dd32b8728ee18db
Ivan Melnikov <ivanempire@gmail.com>
2026-06-03 00:17:00 -0400

    Adding a debug module

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 subprojects {
---
+++ debug/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.ivanempire.debug"
+    compileSdk {
+        version = release(33)
+    }
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin { compilerOptions { jvmTarget = JvmTarget.JVM_17 } }
+
+}
+
+dependencies {
+    implementation(libs.appcompat.v7)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.runner)
+}
